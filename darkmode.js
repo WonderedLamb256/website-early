b46f2df6e50bf1ce354cd73710ebcde27ea9244a
WonderedLamb256 <41864659+WonderedLamb256@users.noreply.github.com>
2020-03-14 19:07:43 -0400

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -6,6 +6,7 @@ function checkForCookie() {
 }
 function enableDarkMode() {
       document.getElementById("1").style.background = "#7a7877";
+      document.getElementById("1").style.color = "#ffffff";
       document.getElementById("2").style.background = "#484848";
       document.getElementById("3").style.background = "#111111";
       document.getElementById("3").style.color = "#cccccc";
