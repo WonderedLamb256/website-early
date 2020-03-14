8e78ae3c2a6fdad8db9baae65cbbccea6bcc0395
WonderedLamb256 <41864659+WonderedLamb256@users.noreply.github.com>
2020-03-14 16:52:56 -0400

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,2 +1,13 @@
+function setDmCookie {
+
+}
+function checkForCookie {
+
+}
 function activateDarkMode() {
+      document.getElementsByClassName("flex-navigation-bar").style.background-color = "#7a7877";
+      document.getElementsByClassName("flex-header").style.background-color = "#484848";
+      document.getElementsByClassName("flex-main").style.background-color = "#111111";
+      document.getElementsByClassName("flex-main").style.color = "#cccccc";
+      setDmCookie();
 }
