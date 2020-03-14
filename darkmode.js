f8c6625a8854c9f046134468f3f9c7283277f8ca
WonderedLamb256 <41864659+WonderedLamb256@users.noreply.github.com>
2020-03-14 19:41:41 -0400

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,8 +1,20 @@
 function setDmCookie() {
-
+      var cookiedate = new Date(); // Spawn a process of date();
+      cookiedate.setTime(cookiedate.getTime() + (365 * 24 * 60 * 60 * 1000));
+      var utccdate = cookiedate.toUTCString();
+      var cookie = browser.cookies.get(details{name:"wldarkmode"});
+      if cookie == "true" {
+            document.cookie = "wldarkmode=false;expires=" + utccdate + ";path=/";
+      }
+      else if cookie == "false" {
+            document.cookie = "wldarkmode=true;expires=" + utccdate + ";path=/";
+      }
 }
 function checkForCookie() {
-
+      var cookie = browser.cookies.get(details{name:"wldarkmode"});
+      if cookie == "true" {
+            enableDarkModeNoSet();
+      }
 }
 function enableDarkMode() {
       document.getElementById("1").style.background = "#7a7877";
@@ -12,4 +24,12 @@ function enableDarkMode() {
       document.getElementById("3").style.color = "#cccccc";
       document.getElementsByTagName("body")[0].setAttribute("bgcolor", "#000000");
       setDmCookie();
+}
+function enableDarkModeNoSet() {
+      document.getElementById("1").style.background = "#7a7877";
+      document.getElementById("1").style.color = "#ffffff";
+      document.getElementById("2").style.background = "#484848";
+      document.getElementById("3").style.background = "#111111";
+      document.getElementById("3").style.color = "#cccccc";
+      document.getElementsByTagName("body")[0].setAttribute("bgcolor", "#000000");
 }
