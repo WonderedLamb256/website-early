3cfc66488408729cccf3e13e6174e270bf2249f0
WonderedLamb256 <41864659+WonderedLamb256@users.noreply.github.com>
2020-03-14 18:14:28 -0400

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -5,9 +5,9 @@ function checkForCookie() {
 
 }
 function enableDarkMode() {
-      document.getElementById("1").style.background-color = "#7a7877";
-      document.getElementById("2").style.background-color = "#484848";
-      document.getElementById("3").style.background-color = "#111111";
+      document.getElementById("1").style.background = "#7a7877";
+      document.getElementById("2").style.background = "#484848";
+      document.getElementById("3").style.background = "#111111";
       document.getElementById("3").style.color = "#cccccc";
       setDmCookie();
 }
